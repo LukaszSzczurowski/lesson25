34b8f048d7f769592c9a9abc3b41215493e0cb51
Łukasz Szczurowski <lszczurowski0@gmail.com>
2020-07-07 22:36:27 +0200

    finish exercise

--- lesson25.sql
+++ lesson25.sql
@@ -1,5 +1,5 @@
 CREATE TABLE pracownik (
-	id INT PRIMARY KEY auto_increment,
+	id BIGINT PRIMARY KEY auto_increment,
 	imie VARCHAR(20),
 	nazwisko VARCHAR(50) NOT NULL,
 	wyplata DECIMAL(8, 2),
@@ -43,26 +43,60 @@ FROM pracownik
 ORDER BY data_urodzenia DESC
 LIMIT 1;
 
-
-
 DROP TABLE pracownik;
 
 CREATE TABLE stanowisko (
-	id INT PRIMARY KEY auto_increment,
+	id BIGINT PRIMARY KEY auto_increment,
 	nazwa_stanowiska VARCHAR(50),
 	opis VARCHAR(200),
-	wyplata_na_danym_stanowisku DOUBLE
+	wyplata_na_danym_stanowisku DECIMAL
 );
 
 CREATE TABLE adres (
-id INT PRIMARY KEY auto_increment,
+id BIGINT PRIMARY KEY auto_increment,
 ulica_numer_domu_mieszkania VARCHAR(50),
 kod_pocztowy VARCHAR(6),
 miejscowosc VARCHAR(50)
 );
 
 CREATE TABLE pracownik (
-	id INT PRIMARY KEY auto_increment,
+	id BIGINT PRIMARY KEY auto_increment,
 	imie VARCHAR(50),
-	nazwisko VARCHAR(50)
+	nazwisko VARCHAR(50),
+    id_adres BIGINT,
+    FOREIGN KEY (id_adres) REFERENCES adres(id),
+    id_stanowisko BIGINT,
+    FOREIGN KEY (id_stanowisko) REFERENCES stanowisko(id)
 );
+
+drop table pracownik;
+
+INSERT INTO stanowisko(nazwa_stanowiska, opis, wyplata_na_danym_stanowisku) VALUES
+('Kierownik', 'Kieruje zasobami', 7277),
+('Specjalista', 'Marketing i reklama', 8500);
+
+SELECT * FROM stanowisko;
+
+INSERT INTO adres(ulica_numer_domu_mieszkania, kod_pocztowy, miejscowosc) VALUES
+('Kwiatowa 27', '11-552', 'Opole'),
+('Liliowa 8/22', '03-001', 'Lublin');
+
+SELECT * FROM adres;
+
+INSERT INTO pracownik(imie, nazwisko, id_adres, id_stanowisko) VALUES
+('Lukasz', 'Rozwalka', 1, 1),
+('Ania', 'Kowalska', 2, 2),
+('Ola', 'Opel', 1, 2);
+
+SELECT * FROM pracownik;
+
+SELECT *
+FROM pracownik p
+JOIN adres a JOIN stanowisko s ON p.id_adres = a.id AND p.id_stanowisko = s.id;
+
+SELECT SUM(wyplata_na_danym_stanowisku) AS suma_wyplat_wszystkich_pracowników
+FROM stanowisko;
+
+SELECT *
+FROM pracownik p JOIN adres a ON p.id_adres = a.id 
+WHERE kod_pocztowy = '11-552';
